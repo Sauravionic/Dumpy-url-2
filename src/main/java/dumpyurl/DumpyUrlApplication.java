1a184bda5ec8f3ae711daccbbc00ccfcf8bf29af
sauravionic <srivastava2000saurav@gmail.com>
2023-06-16 03:56:25 +0530

    fact: first commit

---
+++ src/main/java/dumpyurl/DumpyUrlApplication.java
@@ -0,0 +1,13 @@
+package dumpyurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumpyUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DumpyUrlApplication.class, args);
+    }
+
+}
